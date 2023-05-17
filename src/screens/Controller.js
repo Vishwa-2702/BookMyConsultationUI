37473f5612729abe9f4a968eb7359a0399ded0c9
Vishwa2702 <patelvishwa89@gmail.com>
2023-05-17 19:40:56 +0530

    Completed Capstone Frontend project of bookmyconsultation

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,17 +1,22 @@
 import React from "react";
-import Home from "../screens/home/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Home from "../screens/home/Home";
+import { useAuthContext } from "../hooks/useAuthContext";
+
 const Controller = () => {
-  const baseUrl = "/api/v1/";
+  const baseUrl = "http://localhost:8080/";
+  const {authIsReady} = useAuthContext()
   return (
     <Router>
       <div className="main-container">
+      {authIsReady && (
         <Route
           exact
           path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         />
+        )}
       </div>
     </Router>
   );
